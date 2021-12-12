b37189fdc0b9f2baf25770cb26e95908799c645e
morgunove <morgunove87@gmail.com>
2021-12-12 19:50:53 +0500

    v2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './js/common'
+import './css/main.css'
+import './scss/main.scss'
 window.Vue = require('vue')
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   entry: {
     app: './src/index.js'
@@ -16,10 +17,52 @@ module.exports = {
     },
       {
         test: /\.css$/,
-        loader: 'css-loader',
-      }]
+        use: [
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
+          {
+            loader: "css-loader",
+            options: { sourceMap: true}
+          },
+          {
+            loader: "postcss-loader",
+            options: { sourceMap: true, config: {
+                path: 'src/js/postcss.config.js'
+              }}
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true}
+          },
+          {
+            loader: "postcss-loader",
+            options: { sourceMap: true, config: {
+              path: 'src/js/postcss.config.js'
+              }}
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true}
+          }
+        ]
+      },
+      
+      ]
   },
   devServer: {
     overlay: true
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      // chunkFilename: '[id].css'
+    })
+  ]
 }
